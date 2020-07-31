9b19803d8e3940a6d1bd0e69b2d64928a562938d
Javier Izquierdo <JIZQUIERDO@claro.com.ar>
2020-07-30 21:58:47 -0300

    Updates

---
+++ precache-manifest.4ee93c80634c1933e43d5ea577fc8955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3634373aa4bc7f045d6d88e9912c08f4",
+    "url": "/far8online/index.html"
+  },
+  {
+    "revision": "e1572ae95b8cd157a5fd",
+    "url": "/far8online/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c778527e0bc664dbc138",
+    "url": "/far8online/static/js/2.ebe65a63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/far8online/static/js/2.ebe65a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1572ae95b8cd157a5fd",
+    "url": "/far8online/static/js/main.c188becc.chunk.js"
+  },
+  {
+    "revision": "95506c93b2190af59dd6",
+    "url": "/far8online/static/js/runtime-main.4bd1f257.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/far8online/static/media/logo.5d5d9eef.svg"
+  }
+]);
